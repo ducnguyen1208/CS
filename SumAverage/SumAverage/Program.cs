b46ac81988f90feead66f33bdb4380cf302b629b
ducnguyen1208 <ducnguyen1208@gmail.com>
2019-01-23 12:40:22 -0600

    Added

---
+++ SumAverage/SumAverage/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace SumAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, n4, n5, sum, avg;
+            Console.WriteLine("Enter number");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number");
+            n3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number");
+            n4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number");
+            n5 = Convert.ToInt32(Console.ReadLine());
+            sum = n1 + n2 + n3 + n4 + n5;
+            avg = sum / 5;
+            Console.WriteLine("Sum is {0}", sum);
+            Console.WriteLine("Avg is {0}", avg);
+            Console.ReadLine();
+        }
+    }
+}
