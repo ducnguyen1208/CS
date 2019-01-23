137700344151583d36b6a30e157c9908886ee38a
ducnguyen1208 <ducnguyen1208@gmail.com>
2019-01-23 01:15:36 -0600

    DN

--- program1.cs
+++ program1.cs
@@ -4,6 +4,13 @@ class Program
 {
 	public static void Main()
 	{
+		int num1, num2;
+		Console.WriteLine("Please enter first number");
+		num1 = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Please enter second number");
+		num2 = Convert.ToInt32(Console.ReadLine());
+		int sum = num1 + num2;
+		Console.WriteLine("Addition is: {0}", sum);
 		Console.WriteLine("Good morning");
 		Console.Write("How are you");
 	}
